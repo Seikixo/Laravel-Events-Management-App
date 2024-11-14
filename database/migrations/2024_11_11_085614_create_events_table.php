5e8d5cba281f39610721db138eee8088775c7320
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-11-14 11:00:59 +0800

    Seeder and Factory

--- database/migrations/2024_11_11_085614_create_events_table.php
+++ database/migrations/2024_11_11_085614_create_events_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId(User::class);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->text('description')->nullable();
 
